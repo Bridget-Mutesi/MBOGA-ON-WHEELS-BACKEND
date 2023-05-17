cf1e2022745fbb849d622f3597f19c6bcd3c49cb
Joyeuse KABANYANA <101449143+KABANYANA@users.noreply.github.com>
2023-05-17 07:34:56 +0300

    customer class to sign up and login into the mama mboga grocery store.

--- customer.py
+++ customer.py
@@ -1,38 +1,29 @@
 class Customer:
+ 
     def __init__(self):
-        email= input('enter your email')
-        print('your email is:',email)
-        # self.email=email
+        self.name=none
+        self.email=none
+        self.phone_number=none
+        self.password=none
+        self.location=none
+        self. customers_data=[]
+        
     def signUp(self):
-        name= input('enter your name')
-        print('your name is:',name)
-
+        self.name= input('enter your name')
+        self.email=input('enter your email')
+        self.phone_number= input('enter your phone number')
+        self.password= input('enter your password')
+        self.customers_data.append([self.name,self.email,self.phone_number,self.password])
+        print("signed up successfully")
         
-
-        phone_number= input('enter your phone number')
-        print('your phone number is:',phone_number)
-
-        password= input('enter your password')
-        print('your password is:',password)
-        print(f"your data has been saved")
     
 
-    def login(self,email):
-        login_email= input('enter your email')
-        if(login_email!= email):
-            print("email not correct")
-        p_number= input('enter your phone number')
-        print('your name is:',p_number)
-
-    
-        location= input('enter your location')
-        print('your location is:',location)
-        
-        print('SUCCESSFULY LOGGED IN')
-
-
-    
-customer1=Customer()
-customer1.signUp()
-print("YOUR ACCOUNT HAS BEEN CREATED, YOU CAN LOGIN")
-customer1.login("joyeuse@gmail.com")
+    def login(self):
+        self.email=input('enter your email')
+         self.password= input('enter your password')
+        self.location= input('enter your location')
+        for customer in self.customers_data:
+            if customer [1]==email and customer[2]==password:
+                print(f"WELCOME TO MBOGA ON WHEELS {customer[0]})
+            return
+                print("INVALID EMAIL AND PASSWORD")
